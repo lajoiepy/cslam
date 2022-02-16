5eef414be31462e0cc73cd32ba27556fff7694b0
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2022-02-16 12:18:32 -0500

    Fixed bag recording with scout

--- scripts/sensors/fix_depth_step.py
+++ scripts/sensors/fix_depth_step.py
@@ -4,15 +4,10 @@ from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 bridge = CvBridge()
 
-bag_name = '/home/lajoiepy/Documents/datasets/Poly4eCorridorsRealsense/18jan/Train1_decompressed.bag'
+bag_name = '/home/lajoiepy/Documents/datasets/Poly4eCorridorsRealsense/16Feb2022/Train1.bag'
 
 with rosbag.Bag(bag_name[:-4] + '_fixed.bag', 'w') as outbag:
     for topic, msg, t in rosbag.Bag(bag_name).read_messages():
-        if topic == "/camera/aligned_depth_to_color/image_raw":
-            #msg.step = msg.width * 2
-            msg.encoding = 'mono8'
-            cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding="mono16")
-            msg = bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
-            print(msg.encoding)
-
+        if topic == "/camera/aligned_depth_to_color/image_raw/compressedDepth":
+            msg.format = msg.format + " png"
         outbag.write(topic, msg, t)
